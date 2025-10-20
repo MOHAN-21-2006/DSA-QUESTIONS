08fd71e3de24b2acd0102c708e98e0e700d3cb8d
MOHAN-21-2006 <legendmax08@gmail.com>
2025-10-20 21:19:10 +0530

    Add files via upload

---
+++ circle.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("Enter The Radiaus of Circle :");
+    scanf("%d",&a);
+    int area = 3.14*a*a;
+    printf("The Area of Circle is :%d",area);
+}
